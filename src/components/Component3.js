e91adf8b0898a442ccf8d24a0f7a6936c189de68
domys <domante.stirbyte@gmail.com>
2019-05-23 22:40:51 +0200

    First commit

---
+++ src/components/Component3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Component3() {
+    return (
+        <div style={c3Style}>
+            <h1>Component3</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+    )
+}
+
+const c3Style = {
+    background: 'skyblue',
+    color: 'white',
+    padding: '1.5rem 1.5rem 5rem 1.5rem'
+};
